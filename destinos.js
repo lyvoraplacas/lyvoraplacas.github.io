a08490cb63774404c9e1624d92c9145bcec77b03
lyvoraplacas <grupoantonhy@gmail.com>
2026-09-25 13:42:13 -0300

    Configurar numero de WhatsApp

--- destinos.js
+++ destinos.js
@@ -5,7 +5,7 @@
 
 // 1) Tu WhatsApp (código país + área + número, sin +, sin espacios ni guiones)
 //    Ejemplo Argentina: 549 + 3755 + 123456  ->  "5493755123456"
-var MI_WHATSAPP = "5490000000000";
+var MI_WHATSAPP = "5493741442771";
 
 // 2) Mensaje que llega cuando alguien escanea una placa todavía no vendida
 var MENSAJE = "Hola! Vi una placa de reseñas de Google de Lyvora y quiero la mía";
